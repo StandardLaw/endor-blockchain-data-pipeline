993f048e9a7cd95592daf80b255b646bf2c57d22
Lior Regev <lior@endor.com>
2018-01-29 18:08:52 +0200

    Added fetch_rates script

---
+++ fetch_rates.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+TEMP_FILE=`mktemp`
+curl https://api.coinmarketcap.com/v1/ticker/?limit=0 2>/dev/null > $TEMP_FILE
+aws s3 cp --sse AES256 $TEMP_FILE s3://endor-blockchains/ethereum/rates/Inbox/`date -u +%Y-%m-%d-%H-%M.json`
+rm -f $TEMP_FILE
