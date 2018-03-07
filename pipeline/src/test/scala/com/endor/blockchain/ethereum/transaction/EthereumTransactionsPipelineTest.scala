1af3486fd814f81c96bd80627fa0efef76870010
Yuval Gak <yuval@endor.com>
2018-03-07 11:43:15 +0200

    initial commit of EthereumTransactionsPipleTest

---
+++ pipeline/src/test/scala/com/endor/blockchain/ethereum/transaction/EthereumTransactionsPipelineTest.scala
@@ -0,0 +1,31 @@
+package com.endor.blockchain.ethereum.transaction
+
+import com.endor._
+import com.endor.infra.spark.SparkDriverFunSuite
+import com.endor.infra.{BaseComponent, DIConfiguration}
+import com.endor.storage.sources._
+import org.apache.spark.sql.SparkSession
+
+trait EthereumTransactionsPipelineTestComponent extends EthereumTransactionsPipelineComponent with BaseComponent {
+  override val diConfiguration: DIConfiguration = DIConfiguration.ALL_IN_MEM
+}
+
+
+class EthereumTransactionsPipelineTest extends SparkDriverFunSuite {
+  private def createContainer(): EthereumTransactionsPipelineTestComponent =
+    new EthereumTransactionsPipelineTestComponent {
+      override implicit def spark: SparkSession = EthereumTransactionsPipelineTest.this.spark
+    }
+
+  test("Basic test") {
+    val input_path = getClass.getResource("/com/endor/blockchain/ethereum/blocks/raw/5203600.bin").toString
+    val expected_path = getClass.getResource("/com/endor/blockchain/ethereum/blocks/parsed/5203600.parquet").toString
+    val container = createContainer()
+
+    val config = EthereumTransactionsPipelineConfig(input_path, DataKey(CustomerId("test_customerID"), DataId("test_dataID")))
+    container.driver.run(config)
+    val results = container.datasetStore.loadParquet(config.output.inbox).collect()
+    val expected = spark.read.parquet(expected_path).as[ProcessedTransaction].collect()
+    results should contain theSameElementsAs expected
+  }
+}
