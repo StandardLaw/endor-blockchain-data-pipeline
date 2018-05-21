a16d8542fbae93bc8e302a0fe3b497c342d843e6
Pavel Yuval Gak <yuvalgak@gmail.com>
2018-05-21 12:02:34 +0300

    ElasticsearchDataStatsReporter: adding rates statistics.

--- pipeline/src/main/scala/com/endor/blockchain/ethereum/datastats/ElasticsearchDataStatsReporter.scala
+++ pipeline/src/main/scala/com/endor/blockchain/ethereum/datastats/ElasticsearchDataStatsReporter.scala
@@ -1,24 +1,32 @@
 package com.endor.blockchain.ethereum.datastats
 
+import java.sql.{Date}
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+
 import com.endor.DataKey
+import com.endor.blockchain.ethereum.tokens.AggregatedRates
 import com.endor.blockchain.ethereum.transaction.ProcessedTransaction
 import com.endor.infra.SparkSessionComponent
 import com.endor.storage.dataset.{BatchLoadOption, DatasetStore, DatasetStoreComponent}
 import com.endor.storage.sources._
-import org.apache.spark.sql.{Dataset, Encoder, Encoders, SparkSession}
+import org.apache.spark.sql.{Dataset, Encoder, Encoders, SparkSession, functions => F}
+import org.elasticsearch.hadoop.EsHadoopIllegalArgumentException
 import org.elasticsearch.spark.sql._
 import play.api.libs.json.{Json, OFormat}
 
-final case class BlockStats(blockNumber: Long, date: String, numTx: Int, addresses: Seq[String])
-object BlockStats {
-  implicit val format: OFormat[BlockStats] = Json.format
-  implicit val encoder: Encoder[BlockStats] = Encoders.product
-  val esType: String = "BlockStatsV1"
+import scala.reflect.ClassTag
+import scala.util.{Failure, Success, Try}
 
-  def merge(left: BlockStats, right: BlockStats): BlockStats = {
+final case class BlockStatsV1(blockNumber: Long, date: String, numTx: Int, addresses: Seq[String])
+object BlockStatsV1 {
+  implicit val format: OFormat[BlockStatsV1] = Json.format
+  implicit val encoder: Encoder[BlockStatsV1] = Encoders.product
+
+  def merge(left: BlockStatsV1, right: BlockStatsV1): BlockStatsV1 = {
     require(left.blockNumber == right.blockNumber)
     require(left.date == right.date)
-    BlockStats(left.blockNumber, left.date, right.numTx + left.numTx, (left.addresses ++ right.addresses).distinct)
+    BlockStatsV1(left.blockNumber, left.date, right.numTx + left.numTx, (left.addresses ++ right.addresses).distinct)
   }
 }
 
@@ -29,22 +37,33 @@ object DatasetDefinition {
 }
 
 final case class ElasticsearchDataStatsConfig(ethereumTxDefinition: DatasetDefinition[ProcessedTransaction],
+                                              etherRatesDefinition: DatasetDefinition[AggregatedRates],
                                               elasticsearchIndex: String, esHost: String, esPort: Int)
 
 object ElasticsearchDataStatsConfig {
   implicit val format: OFormat[ElasticsearchDataStatsConfig] = Json.format[ElasticsearchDataStatsConfig]
 }
 
+trait EsType[T] {
+  def esType: String
+}
+
+object EsType {
+  implicit def esType[T](implicit classTag: ClassTag[T]): EsType[T] = new  EsType[T] {
+    override val esType: String = classTag.runtimeClass.getSimpleName
+  }
+}
+
 class ElasticsearchDataStatsReporter()
                                     (implicit spark: SparkSession, datasetStore: DatasetStore) {
-
-
   def run(config: ElasticsearchDataStatsConfig): Unit = {
-    val results = process(config)
-    storeToES(config, results)
+    val etherData = processEthereum(config)
+    storeToES(config, etherData)
+    val ratesData = processRates(config)
+    storeToES(config, ratesData)
   }
 
-  private def process(config: ElasticsearchDataStatsConfig): Dataset[BlockStats] = {
+  private def processEthereum(config: ElasticsearchDataStatsConfig): Dataset[BlockStatsV1] = {
     val sess = spark
     import sess.implicits._
     val onBoarded = datasetStore.loadParquet(config.ethereumTxDefinition.dataKey.onBoarded,
@@ -52,23 +71,51 @@ class ElasticsearchDataStatsReporter()
     onBoarded
       .map(
         tx =>
-          BlockStats(tx.blockNumber, tx.timestamp.toInstant.toString, 1, Seq(tx.sendAddress))
+          BlockStatsV1(tx.blockNumber, tx.timestamp.toInstant.toString, 1, Seq(tx.sendAddress))
       )
       .groupByKey(_.blockNumber)
-      .reduceGroups(BlockStats.merge _)
+      .reduceGroups(BlockStatsV1.merge _)
       .map(_._2)
       // transactions are counted twice.
-      .map(results => BlockStats(results.blockNumber, results.date, results.numTx/2, results.addresses))
+      .map(results => BlockStatsV1(results.blockNumber, results.date, results.numTx/2, results.addresses))
+  }
+
+  private def processRates(config: ElasticsearchDataStatsConfig)
+                          (implicit esType: EsType[AggregatedRates]): Dataset[AggregatedRates] = {
+    val sess = spark
+    import sess.implicits._
+    val millisInDay = 86400000 // 3600 * 24 *  1000
+    val yesterday = new Date(Instant.now().truncatedTo(ChronoUnit.DAYS).toEpochMilli - millisInDay)
+    val maxDateInES = Try {
+      spark.esDF(s"${config.elasticsearchIndex}/${esType.esType}",createEsConfig(config))
+        .withColumn("date", F.to_date($"date","yyyy-MM-dd"))
+        .agg(F.max("date"))
+        .as[Option[Date]]
+        .head()
+    } match  {
+      case Success(value) => value
+      case Failure(_: EsHadoopIllegalArgumentException) => None
+      case Failure(err) => throw err
+    }
+    val loaded = datasetStore.loadParquet(config.etherRatesDefinition.dataKey.onBoarded,
+      batchLoadOption = config.etherRatesDefinition.batchLoadOption)
+    maxDateInES.map(maxDate => loaded.filter($"date" > maxDate)).getOrElse(loaded)
+      .filter($"date" <= yesterday)
   }
 
 
-  private def storeToES(config: ElasticsearchDataStatsConfig, results: Dataset[BlockStats]): Unit = {
-    results.saveToEs(s"${config.elasticsearchIndex}/${BlockStats.esType}",
-      Map(
-        "es.nodes" -> config.esHost,
-        "es.port" -> config.esPort.toString,
-        "es.nodes.wan.only" -> true.toString
-      )
+  private def createEsConfig(config: ElasticsearchDataStatsConfig): Map[String, String] =
+    Map(
+      "es.nodes" -> config.esHost,
+      "es.port" -> config.esPort.toString,
+      "es.nodes.wan.only" -> true.toString
+    )
+
+  private def storeToES[T: Encoder: ClassTag](config: ElasticsearchDataStatsConfig, results: Dataset[T])
+                                             (implicit esType: EsType[T]): Unit = {
+    // TODO: add publish_timestamp.
+    results.saveToEs(s"${config.elasticsearchIndex}/${esType.esType}",
+      createEsConfig(config)
     )
   }
 }
--- pipeline/src/test/scala/com/endor/blockchain/ethereum/datastats/ElasticsearchDataStatsReporterTest.scala
+++ pipeline/src/test/scala/com/endor/blockchain/ethereum/datastats/ElasticsearchDataStatsReporterTest.scala
@@ -1,7 +1,8 @@
 package com.endor.blockchain.ethereum.datastats
 
-import java.sql.Timestamp
+import java.sql.{Date, Timestamp}
 
+import com.endor.blockchain.ethereum.tokens.AggregatedRates
 import com.endor.blockchain.ethereum.transaction.ProcessedTransaction
 import com.endor.infra.spark.SparkDriverFunSuite
 import com.endor.infra.{BaseComponent, DIConfiguration}
@@ -12,7 +13,7 @@ import com.sksamuel.elastic4s.embedded.LocalNode
 import com.sksamuel.elastic4s.http.ElasticDsl._
 import com.sksamuel.elastic4s.http.search.SearchResponse
 import com.sksamuel.elastic4s.http.{HttpClient, Response}
-import org.apache.spark.sql.{SparkSession, functions => F}
+import org.apache.spark.sql.{Dataset, SparkSession, functions => F}
 import play.api.libs.json.{Json, Reads}
 
 trait ElasticsearchDataStatsReporterTestComponent extends ElasticsearchDataStatsReporterComponent with BaseComponent {
@@ -20,6 +21,7 @@ trait ElasticsearchDataStatsReporterTestComponent extends ElasticsearchDataStats
 }
 
 class ElasticsearchDataStatsReporterTest extends SparkDriverFunSuite {
+
   case class Node(client: HttpClient, ip: String, port: String)
 
   private def clientResponseToObjects[T: Reads](response: Response[SearchResponse]): Seq[T] = {
@@ -38,59 +40,125 @@ class ElasticsearchDataStatsReporterTest extends SparkDriverFunSuite {
       override implicit def spark: SparkSession = ElasticsearchDataStatsReporterTest.this.spark
     }
 
-  test("Test blocks 5609255-5609260") {
+  test("Test tansactions blocks 5609255-5609260") {
     val sess = spark
     import sess.implicits._
     val container = createContainer()
     System.setProperty("es.set.netty.runtime.available.processors", "false")
     val Node(client, ip, port) = createNode()
     val batchId = "my_batch"
-    val dataKey = DataKey[ProcessedTransaction](CustomerId("testCustomer"), DataId("testDs") )
-    val config = ElasticsearchDataStatsConfig(DatasetDefinition(dataKey, BatchLoadOption.UseExactly(Seq(batchId))),
-      "test1", ip, port.toInt)
+    val dataKeyTransaction = DataKey[ProcessedTransaction](CustomerId("testCustomer"), DataId("testDsTransaction"))
+    val dataKeyRates = DataKey[AggregatedRates](CustomerId("testCustomer"), DataId("testDsRates"))
+    val config = ElasticsearchDataStatsConfig(DatasetDefinition(dataKeyTransaction, BatchLoadOption.UseExactly(Seq(batchId))),
+      DatasetDefinition(dataKeyRates, BatchLoadOption.UseExactly(Seq(batchId))), "test1", ip, port.toInt)
+
     val inputPath = this.getClass.getResource("/com/endor/blockchain/ethereum/blocks/parsed/5609255-5609260.parquet").toString
     val inputDs = spark.read.parquet(inputPath).as[ProcessedTransaction]
-    container.datasetStore.storeParquet(dataKey.onBoarded,
+    container.datasetStore.storeParquet(dataKeyTransaction.onBoarded,
       inputDs.withColumn("batch_id", F.lit(batchId)).as[ProcessedTransaction])
+    container.datasetStore.storeParquet(dataKeyRates.onBoarded,
+      spark.emptyDataset[AggregatedRates].withColumn("batch_id", F.lit(batchId)).as[AggregatedRates])
+
+
     container.driver.run(config = config)
     val ts = new Timestamp(0).toInstant.toString
     val selfComputed = inputDs
       .groupByKey(_.blockNumber)
       .mapGroups {
         (blockNumber, txIt) =>
-          val a = txIt.foldLeft(BlockStats(blockNumber, ts, 0, Seq.empty)) {
+          val a = txIt.foldLeft(BlockStatsV1(blockNumber, ts, 0, Seq.empty)) {
             case (acc, tx) => acc.copy(numTx = acc.numTx + 1, date = tx.timestamp.toInstant.toString, addresses = acc.addresses :+ tx.sendAddress)
           }
           a.copy(numTx = a.numTx / 2, addresses = a.addresses.distinct)
       }
+
     val expected = selfComputed.collect()
     val resp = client.execute {
-      searchWithType(config.elasticsearchIndex / BlockStats.esType) limit expected.length + 1
+      searchWithType(config.elasticsearchIndex / implicitly[EsType[BlockStatsV1]].esType) limit expected.length + 1
     }.await
-    resp.isRight should be (true)
+    resp.isRight should be(true)
     resp match {
       case Right(success) =>
-        val results = clientResponseToObjects[BlockStats](success)
+        val results = clientResponseToObjects[BlockStatsV1](success)
         results should contain theSameElementsAs expected
       case _ =>
     }
+    client.close()
   }
 
-  ignore("REMOTE AWS Test blocks 5609255-5609260") {
+  private def runAndCompare(inputDs: Dataset[AggregatedRates], container: ElasticsearchDataStatsReporterTestComponent,
+                            config: ElasticsearchDataStatsConfig, dataKeyRates: DataKey[AggregatedRates],
+                            dateStr: String, client: HttpClient, batchId: String): Unit = {
+    val sess = spark
+    import sess.implicits._
+    val date = Date.valueOf(dateStr)
+    val inputDSFirst = inputDs.filter($"date" < date)
+    container.datasetStore.storeParquet(dataKeyRates.onBoarded,
+      inputDSFirst.withColumn("batch_id", F.lit(batchId)).as[AggregatedRates])
+    container.driver.run(config = config)
+
+
+    val respFirst = client.execute {
+      searchWithType(config.elasticsearchIndex / implicitly[EsType[AggregatedRates]].esType) limit inputDSFirst.count().toInt + 100
+    }.await
+    respFirst.isRight should be(true)
+    respFirst match {
+      case Right(success) =>
+        val results1 = clientResponseToObjects[AggregatedRates](success)
+        results1 should contain theSameElementsAs inputDSFirst.collect()
+      case _ =>
+    }
+  }
+  test("Basic: Test rates single day") {
     val sess = spark
     import sess.implicits._
     val container = createContainer()
     System.setProperty("es.set.netty.runtime.available.processors", "false")
-    val ip = "https://search-yuvaltest-rhmhcat7ojnadroc2pnqleil5q.us-east-1.es.amazonaws.com"
-    val port = 443
+    val Node(client, ip, port) = createNode()
+    val batchId = "my_batch_basic"
+    val dataKeyTransaction = DataKey[ProcessedTransaction](CustomerId("testCustomerBasic: Test rates single Day"), DataId("testDsTransactionBasic: Test rates single Day"))
+    val dataKeyRates = DataKey[AggregatedRates](CustomerId("testCustomer1"), DataId("testDsRates1"))
+    val inputPath = this.getClass.getResource("/com/endor/blockchain/ethereum/rates/single_day.parquet").toString
+    val inputDs = spark.read.parquet(inputPath)
+      .withColumn("date", F.to_date($"date", "yyyy-MM-dd"))
+      .as[AggregatedRates]
+
+    val configFirst = ElasticsearchDataStatsConfig(DatasetDefinition(dataKeyTransaction, BatchLoadOption.UseExactly(Seq(batchId))),
+      DatasetDefinition(dataKeyRates, BatchLoadOption.UseExactly(Seq(batchId))), "test1", ip, port.toInt)
+    container.datasetStore.storeParquet(dataKeyTransaction.onBoarded,
+      spark.emptyDataset[ProcessedTransaction].withColumn("batch_id", F.lit(batchId)).as[ProcessedTransaction])
+    runAndCompare(inputDs, container, configFirst, dataKeyRates, "2019-01-01", client, batchId)
+    client.close()
+
+  }
+
+  test("Advanced: Test rates with Maxdate") {
+    // init
+    val sess = spark
+    import sess.implicits._
+    val container = createContainer()
+    System.setProperty("es.set.netty.runtime.available.processors", "false")
+    val Node(client, ip, port) = createNode()
     val batchId = "my_batch"
-    val dataKey = DataKey[ProcessedTransaction](CustomerId("testCustomer"), DataId("testDs") )
-    val config = ElasticsearchDataStatsConfig(DatasetDefinition(dataKey, BatchLoadOption.UseExactly(Seq(batchId))),
-      "spark_test1", ip, port.toInt)
-    val inputPath = this.getClass.getResource("/com/endor/blockchain/ethereum/blocks/parsed/5609255-5609260.parquet").toString
-    val inputDs = spark.read.parquet(inputPath).as[ProcessedTransaction]
-    container.datasetStore.storeParquet(dataKey.onBoarded,
-      inputDs.withColumn("batch_id", F.lit(batchId)).as[ProcessedTransaction])
-    container.driver.run(config = config)
+    val dataKeyTransaction = DataKey[ProcessedTransaction](CustomerId("testCustomerAdvanced: Test rates with Maxdate"),
+      DataId("testDsTransactionAdvanced: Test rates with Maxdate"))
+    val dataKeyRates = DataKey[AggregatedRates](CustomerId("testCustomer"), DataId("testDsRates"))
+    val inputPath = this.getClass.getResource("/com/endor/blockchain/ethereum/rates/2018-05-15-rates.parquet").toString
+    val inputDs = spark.read.parquet(inputPath)
+      .withColumn("date", F.to_date($"date", "yyyy-MM-dd"))
+      .as[AggregatedRates]
+
+
+    val configFirst = ElasticsearchDataStatsConfig(DatasetDefinition(dataKeyTransaction, BatchLoadOption.UseExactly(Seq(batchId))),
+      DatasetDefinition(dataKeyRates, BatchLoadOption.UseExactly(Seq(batchId))), "test1", ip, port.toInt)
+    container.datasetStore.storeParquet(dataKeyTransaction.onBoarded,
+      spark.emptyDataset[ProcessedTransaction].withColumn("batch_id", F.lit(batchId)).as[ProcessedTransaction])
+    runAndCompare(inputDs, container, configFirst, dataKeyRates, "2016-01-01", client, batchId)
+
+
+    val configSecond = ElasticsearchDataStatsConfig(DatasetDefinition(dataKeyTransaction, BatchLoadOption.UseExactly(Seq(batchId))),
+      DatasetDefinition(dataKeyRates, BatchLoadOption.UseExactly(Seq(batchId))), "test2", ip, port.toInt)
+    runAndCompare(inputDs, container, configSecond, dataKeyRates, "2017-01-01", client, batchId)
+    client.close()
   }
 }
